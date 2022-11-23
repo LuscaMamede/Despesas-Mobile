04dbe4fcb4a937f678bcb4fab9fbdb6764be250f
LuscaMamede <eumesmolucassilva@gmail.com>
2022-11-23 14:29:13 -0300

    Commit transaction

--- lib/components/transaction_list.dart
+++ lib/components/transaction_list.dart
@@ -1,63 +1,62 @@
 import 'package:flutter/material.dart';
-import 'package:intl/intl.dart';
 import '../models/transaction.dart';
+import 'package:intl/intl.dart';
 
-// ignore: must_be_immutable
 class TransactionList extends StatelessWidget {
   final List<Transaction> transactions;
 
-  const TransactionList({Key? key, required this.transactions})
-      : super(key: key);
+  const TransactionList(this.transactions, {Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Column(
-        children: transactions.map((tr) {
-      return Card(
-        child: Row(
-          children: [
-            Container(
-              margin: const EdgeInsets.symmetric(
-                horizontal: 15,
-                vertical: 10,
-              ),
-              decoration: BoxDecoration(
-                border: Border.all(
-                  color: Colors.purple,
-                  width: 2,
+      children: transactions.map((tr) {
+        return Card(
+          child: Row(
+            children: [
+              Container(
+                margin: const EdgeInsets.symmetric(
+                  horizontal: 15,
+                  vertical: 10,
                 ),
-              ),
-              padding: const EdgeInsets.all(10),
-              child: Text(
-                'R\$ ${tr.value.toStringAsFixed(2)}',
-                style: const TextStyle(
-                  fontWeight: FontWeight.bold,
-                  fontSize: 20,
-                  color: Colors.purple,
+                decoration: BoxDecoration(
+                  border: Border.all(
+                    color: Colors.purple,
+                    width: 2,
+                  ),
                 ),
-              ),
-            ),
-            Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Text(
-                  tr.title,
+                padding: const EdgeInsets.all(10),
+                child: Text(
+                  'R\$ ${tr.value.toStringAsFixed(2)}',
                   style: const TextStyle(
-                    fontSize: 16,
                     fontWeight: FontWeight.bold,
+                    fontSize: 20,
+                    color: Colors.purple,
                   ),
                 ),
-                Text(
-                  DateFormat('d MMM y').format(tr.date),
-                  style: const TextStyle(
-                    color: Colors.grey,
+              ),
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    tr.title,
+                    style: const TextStyle(
+                      fontSize: 16,
+                      fontWeight: FontWeight.bold,
+                    ),
                   ),
-                ),
-              ],
-            ),
-          ],
-        ),
-      );
-    }).toList());
+                  Text(
+                    DateFormat('d MMM y').format(tr.date),
+                    style: const TextStyle(
+                      color: Colors.grey,
+                    ),
+                  ),
+                ],
+              ),
+            ],
+          ),
+        );
+      }).toList(),
+    );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
-import 'package:expenses/components/transaction_list.dart';
 import 'package:flutter/material.dart';
-import 'models/transaction.dart';
+import 'components/transaction_user.dart';
 
 main() => runApp(const ExpensesApp());
 
@@ -8,26 +7,12 @@ class ExpensesApp extends StatelessWidget {
   const ExpensesApp({Key? key}) : super(key: key);
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(home: MyHomePage());
+    return const MaterialApp(home: MyHomePage());
   }
 }
 
 class MyHomePage extends StatelessWidget {
-  MyHomePage({Key? key}) : super(key: key);
-  final transactions = [
-    Transaction(
-      id: 't1',
-      title: 'Novo Tênis de Corrida',
-      value: 310.76,
-      date: DateTime.now(),
-    ),
-    Transaction(
-      id: 't2',
-      title: 'Conta de Luz',
-      value: 211.30,
-      date: DateTime.now(),
-    ),
-  ];
+  const MyHomePage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
@@ -37,49 +22,15 @@ class MyHomePage extends StatelessWidget {
       ),
       body: Column(
         crossAxisAlignment: CrossAxisAlignment.stretch,
-        children: [
-          const SizedBox(
+        children: const [
+          SizedBox(
             child: Card(
               color: Colors.blue,
               child: Text('Gráfico'),
               elevation: 5,
             ),
           ),
-          TransactionList(transactions: transactions),
-          Card(
-            elevation: 5,
-            child: Padding(
-              padding: const EdgeInsets.all(10),
-              child: Column(
-                children: [
-                  const TextField(
-                    decoration: InputDecoration(
-                      labelText: 'Título',
-                    ),
-                  ),
-                  const TextField(
-                    decoration: InputDecoration(
-                      labelText: 'Valor (R\$)',
-                    ),
-                  ),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.end,
-                    children: <Widget>[
-                      TextButton(
-                        child: const Text(
-                          'Nova Transação',
-                          style: TextStyle(
-                            color: Colors.purple,
-                          ),
-                        ),
-                        onPressed: () {},
-                      )
-                    ],
-                  ),
-                ],
-              ),
-            ),
-          )
+          TransactionUser(),
         ],
       ),
     );
